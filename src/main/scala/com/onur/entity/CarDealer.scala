354bb91b001752cc03b42cf730ebf4bf41af65cc
Onur_Dincol <onur_dincol@epam.com>
2017-10-15 16:50:25 +0200

    New Project

---
+++ src/main/scala/com/onur/entity/CarDealer.scala
@@ -0,0 +1,7 @@
+package com.onur.entity
+
+/**
+  * Created by Onur_Dincol on 10/15/2017.
+  */
+case class CarDealer(dateTime:String, carMaker:String, carModel:String, customerName:String, customerSurname:String, customerEmail:String)
+
